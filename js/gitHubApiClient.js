31ce766f5efb0eb488d9ed5d50f400a28fe35e29
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-11-19 12:51:21 -0200

    Atualizando exibição dos repositorios

--- js/gitHubApiClient.js
+++ js/gitHubApiClient.js
@@ -1,18 +1,34 @@
 var divRepositoriosDeck = $("#repoCardDeck");
 var str = "";
 
-function createRepoCard(repoName, repoDesc, repoUrl){
+function createRepoCard(repoName, repoDesc, repoUrl, repoForks, repoStars, repoWatchers){
     str +=
     "<div class='col-sm-4  d-flex align-items-stretch mt-3'>"
     +" <div class='card border-primary text-center'>"
-    +"  <div class='card-header'>" + repoName + "</div>"
-    +"   <div class='card-body'>"
-    // +"     <h4 class='card-title'>" 
-    // +       repoName 
-    // +"     </h4>"
-    +"     <p class='card-text'>"
-    +       repoDesc
-    +"     </p>"
+    +"  <H4 class='card-header'>" 
+    +       repoName
+    +"  </H4>"
+    +"  <div class='card-body'>"
+    +"      <p>"
+    +          repoDesc
+    +"      </p>"
+    +"      <small class='text-muted' style='margin:20px;'>"
+    +"          <span class='fa fa-1x fa-star mb-3 sr-icons' style='margin:5px;'>"
+    +"              <span style='margin:3px;'>"
+    +                   repoStars 
+    +"              </span>"
+    +"          </span>"
+    +"         <span class='fa fa-1x fa-eye mb-3 sr-icons' style='margin:5px;'>"
+    +"             <span style='margin:3px;'>"
+    +                repoWatchers 
+    +"              </span>"
+    +"         </span>"
+    +"         <span class='fa fa-1x fa-code-fork mb-3 sr-icons' style='margin:5px;'>"
+    +"             <span style='margin:3px;'>"
+    +                repoForks
+    +"              </span>"
+    +"         </span>"   
+    +"       </small>"
     +"   </div>"
     +"   <div class='card-footer'>"
     //+"     <small class='text-muted'>Last updated 3 mins ago</small>"
@@ -31,23 +47,25 @@ $(document).ready(function(){
     
         console.log(data);
 
-        
+        //Criando os cartoes dos repositorios
         var i = 1;
         str +="<div class='row'>";
-        //Apresentando os nomes e descricoes
+        
         data.forEach(repo => {
-            console.log(repo.name + ", Descrição: " + repo.description);
+            console.log(
+                repo.name 
+                + ", Descrição: " + repo.description 
+                + ", Forks: " + repo.forks_count 
+                + ", Stars: " + repo.stargazers_count
+                + ", Watchers: " + repo.watchers_count);
             if(i%3 == 0){
-                // debugger;
-                createRepoCard(repo.name, repo.description, repo.html_url);
+                createRepoCard(repo.name, repo.description, repo.html_url, repo.forks_count, repo.stargazers_count, repo.watchers_count);
                 str +="</div>";
                 str +="<div class='row'>";
             }
             else{
-                // debugger;
-                createRepoCard(repo.name, repo.description, repo.html_url);                
+                createRepoCard(repo.name, repo.description, repo.html_url, repo.forks_count, repo.stargazers_count, repo.watchers_count);
             }
-            
             i++;
         });
         str +="</div>";
